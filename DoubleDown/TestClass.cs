c4b56a059c697f5f2d1bd348b2e23c23ca416ccf
Kevin Bost <kitokeboo@gmail.com>
2017-07-21 22:11:54 -0700

    Initial commit.

---
+++ DoubleDown/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoubleDown
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var dd = new DoubleDown();
+
+            int invocationCount = dd.Get<int>();
+            
+            Assert.AreEqual(2, invocationCount);
+        }
+    }
+
+    public class DoubleDown
+    {
+        public T Get<T>()
+        {
+            if (GetInternal() is T result)
+            {
+                return result;
+            }
+            return default(T);
+        }
+
+        private int _invokeCount;
+        private object GetInternal()
+        {
+            return ++_invokeCount;
+        }
+    }
+}
